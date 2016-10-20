e159bb90bba3781a929b8ad78f4b441b31a52207
eswai-loper <eswai@foxmail.com>
2016-10-20 18:27:06 +0800

    image cropping

---
+++ ImageCrop.m
@@ -0,0 +1,39 @@
+function ImageCrop(src_dir,dst_dir,format,spacing)
+% spacing=256;
+% src_dir='TESTIMG';
+% dst_dir='image_cut';
+% format='tif';
+
+subfolders=dir(src_dir);
+for ii=1:length(subfolders)
+    subname=subfolders(ii).name;
+    % ignore current dir and father dir
+    if ~strcmp(subname,'.')&&~strcmp(subname,'..')
+        frames=dir(fullfile(src_dir,subname,['*.',format]));
+        imgnum=length(frames);
+        dstpath=fullfile(dst_dir,subname);
+        if ~isdir(dstpath)
+            mkdir(dstpath);
+        end
+        for jj=1:imgnum
+            imgpath=fullfile(src_dir,subname,frames(jj).name);
+            I=imread(imgpath);
+            row=floor(size(I,1)/spacing);
+            col=floor(size(I,2)/spacing);
+            % Start Croping
+            for rr=1:row
+                for cc=1:col
+                    rect=[(cc-1)*spacing+1,(rr-1)*spacing+1,spacing-1,spacing-1];
+                    newI=imcrop(I,rect);
+                    newname=[frames(jj).name,'_',num2str((rr-1)*col+cc),['.',format]];
+                    newpath=fullfile(dstpath,newname);
+                    imwrite(newI,newpath);
+                end
+            end
+        end
+    end % end of if
+end
+end
+
+
+
